e27a88e354e9c302dd1d6a06356a0c7592061145
Tiggy Tamal <Tiggy Tamal@192.168.1.29>
2016-12-12 00:27:14 +0100

    Poi

--- Redmine/src/bean/ExcelBean.java
+++ Redmine/src/bean/ExcelBean.java
@@ -1,9 +1,19 @@
 package bean;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean (name = "excel")
 @RequestScoped
@@ -12,6 +22,26 @@ public class ExcelBean implements Serializable
 
     private static final long serialVersionUID = 1L;
     
-    
+ // Attribut pour l'import
+    private UploadedFile file;
 
+    public void test()
+    {
+        try
+        {
+            Workbook wb2 = WorkbookFactory.create(new File("MyExcel.xls"));
+            Workbook wb = WorkbookFactory.create(new FileInputStream("MyExcel.xlsx"));
+        }
+        catch(InvalidFormatException | EncryptedDocumentException | IOException e)
+        {
+            
+        }
+    }
+    
+    public void charger(FileUploadEvent event) throws EncryptedDocumentException, InvalidFormatException, IOException
+    {
+        file = event.getFile();
+        Workbook wb = WorkbookFactory.create(new File(file.getFileName()));
+    }
+   
 }
